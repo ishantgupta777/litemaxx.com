5e062449535ddbb0cdb5a57c672911d1610a5f4a
ishant <ishantgupta777@gmail.com>
2019-08-10 05:28:05 +0530

    home page finish

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+//-------------types.js-------------------------------
+
+var typed = new Typed('.type', {
+    stringsElement: '#typed-strings',
+    typeSpeed : 50,
+    backSpeed : 50,
+    loop : true,
+    backDelay : 2000
+});

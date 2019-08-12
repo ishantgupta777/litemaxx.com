6bcfd4bf194cce71d56c0b6e8cb21271563fa8e1
ishant <ishantgupta777@gmail.com>
2019-08-12 08:07:27 +0530

    pricing section added

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,60 @@ var typed = new Typed('.type', {
     loop : true,
     backDelay : 2000
 });
+
+//--------------nav colour change------------------
+$(document).scroll(function() {
+   if($(document).scrollTop() >  50){
+       $('nav').css({
+           'background':'#202123',
+           'transition' : 'all 300ms ease-in-out'
+        });
+   }else{
+    $('nav').css({
+        'background':'transparent',
+        'transition' : 'all 300ms ease-in'
+     });
+   }
+})
+
+//------------prices slider----------------------------
+var swiper = new Swiper('.pricing-grid', {
+   slidesPerView: 4,
+   loop: true,
+   paginationClickable: true,
+   pagination: '.swiper-pagination',
+   paginationType: 'fraction',
+   speed: 1000,
+   roundLengths: true,
+   parallax: true,
+   keyboardControl: true,
+   observer : true,
+   observeParents : true,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   breakpoints: {
+       0: {
+           slidesPerView: 1,
+       },
+       580: {
+           slidesPerView: 1,
+       },
+       768: {
+           slidesPerView: 1,
+       },
+       900: {
+           slidesPerView: 2,
+       },
+       1100: {
+           slidesPerView: 2,
+       },
+       1600: {
+           slidesPerView: 3,
+       },
+       1800: {
+           slidesPerView: 4,
+       }
+   }
+});

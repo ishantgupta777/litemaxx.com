c681c071eee14a14b73e676908571482cb87cb64
ishant <ishantgupta777@gmail.com>
2019-08-15 17:39:52 +0530

    backend added but database url adding remaining

--- public/js/index.js
+++ public/js/index.js
@@ -103,3 +103,54 @@ $('form').on('change',()=>{
 if($(window).width()<700){
     $('<br>').insertBefore('#dynamic-text');
 }
+
+//------------------contact form submission backend-------------------
+$(document).ready(()=>{
+    document.querySelector('#contact-form').addEventListener('submit',(e)=>{
+      e.preventDefault()
+  
+      $.ajax({
+          type: 'POST',
+          url: $("#contact-form").attr("action"),
+          data: $("#contact-form").serialize(), 
+        });
+        document.querySelector('#contact-form').reset()
+        var footerHeight =  document.querySelector('.footer').offsetHeight;
+        $('.footer').html(`<h1 class="thankyou centerFlex" data-aos="fade-left" style="color:white;text-align:center;width:75%;height:${footerHeight}px; transition: 200ms ease-in-out; ">Thank you for your message! We will contact you as soon as possible</h1>`)
+        setTimeout(()=>{
+          $('.footer').html(`  <div class="connect">
+          <h1>connect with us</h1>
+          <img src="./img/arrow-55-64.png" alt="">
+          <div class="hrtag hrtag4"></div>
+          <div class="social-div">
+                  <div class="social">
+                          <a> <img src="./img/insta.png" alt=""> </a>
+                          <a> <img src="./img/mail-64.png" alt=""> </a>
+                          <a> <img src="./img/PNGIX.com_logo-whatsapp-png_494364.png" alt=""> </a>
+                      </div>
+          </div>
+      </div>
+      <div class="contact">
+          <form id="contact-form" action="/contact">
+              <div>
+                      <input type="text" class="name" name="name" placeholder="">
+                      <label for="name" id="name" class="label">Your Name</label>
+              </div>
+              <div>
+                      <input type="email" class="email" name="email" required placeholder="">
+                      <label id="email" for="email">Email</label>
+              </div>
+              <div>
+                      <input type="text" class="phone" name="phone">
+                      <label id="phone" for="phone">Phone Number</label>
+              </div>
+              <div>
+                      <textarea name="message" class="message" required cols="30" rows="10" placeholder=""></textarea>
+                      <label id="message" for="message">Your Message</label>
+              </div>
+              <button type="submit">SUBMIT</button>
+          </form>
+      </div>`)
+        },3000)
+  })
+  })
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const bodyParser = require('body-parser')
+const sendMail = require('./email')
+
+const publicPath = path.join(__dirname,'../public')
+const indexPath = path.join(__dirname,'../public/index.html')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(publicPath))
+
+const port = process.env.PORT
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on port ${port}`)
+})
+
+mongoose.connect( process.env.MONGODB_URL ,{
+    useNewUrlParser : true,
+    useCreateIndex : true
+});
+
+const userSchema = mongoose.Schema({
+    name : {
+        type : String
+    },
+    email : {
+        type : String,
+        required  : true,
+        toLowetCase : true,
+        trim : true
+    },
+    phone : {
+        type : String
+    },
+    message : {
+        type : String
+    }
+})
+
+const User = mongoose.model('user',userSchema)
+
+app.get('/',(req,res)=>{
+    res.sendFile(publicPath)
+})
+
+app.post('/contact',(req,res)=>{
+    const userMsg = new User(req.body)
+    userMsg.save()
+    sendMail.sendWelcomeEmail(req.body)
+    res.send()
+})
